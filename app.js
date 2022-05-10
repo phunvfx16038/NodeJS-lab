8e24490e82eff2e7690f28891f2723902e44f37a
phunguyen25 <phunv@devcamp.edu.vn>
2022-05-10 11:38:08 +0700

    commit code lab2.04

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes.handler);
+const express = require("express");
 
-server.listen(3000);
+const app = express();
+
+app.use("/", (req, res, next) => {
+  console.log("This middleware always run!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("The 'Add product' page! ");
+  res.send("<h1>The Add product page!</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
