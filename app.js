5c65c36c7184b1873a9cbcb5699bb5428bff99b6
phunguyen25 <phunv@devcamp.edu.vn>
2022-05-11 10:32:53 +0700

    commit code lab2.05 -> 2.08

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form method='POST' action='/product'><input type='text' name='input'><button>Submit</button></form>"
-  );
-});
+app.use(adminRouter);
+app.use(shopRouter);
 
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
 });
 
 app.listen(3000);
